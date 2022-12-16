b9b3373eee838db5334e9570e1143322a9775fb9
branhamsnowflaketraining <119965960+branhamsnowflaketraining@users.noreply.github.com>
2022-12-16 12:44:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,6 +53,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-insert into fruit_load_list values ('from streamlit');
+insert into fruit_load_list values (SELECT 'from streamlit');
 
 
